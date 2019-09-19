633df7e5923e22377808f45bc904303fc75bbf72
Rhett <thedovahbrine@gmail.com>
2019-09-19 15:38:48 -0600

    Added unity file

---
+++ Flight Quest Sandbox/Assets/Scripts/PassengerQue.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent((typeof(UnityEngine.AI.NavMeshAgent)))]
+public class PassengerQue : MonoBehaviour
+{
+
+}
