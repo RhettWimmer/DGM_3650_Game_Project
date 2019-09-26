7583b7bb3710847ae6fa89770beaa5e1f9b5301f
Rhett <thedovahbrine@gmail.com>
2019-09-26 13:27:45 -0600

    Working

---
+++ Flight Quest Sandbox/Assets/Scripts/AnimatorMachine.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimatorMachine : MonoBehaviour
+{
+    public GameObject RunAnim;
+    public GameObject StopAnim;
+
+    public bool Running;
+    public bool Jumping;
+
+
+    private void Update()
+    {
+        if (Running == true && Jumping == false)
+        {
+            RunAnim.SetActive(true);
+            StopAnim.SetActive(false);
+        }
+
+        if (Jumping == true && Running == false)
+        {
+            RunAnim.SetActive(false);
+            StopAnim.SetActive(true);
+        }
+        
+        print(Running);
+        print(Jumping);
+    }
+}
