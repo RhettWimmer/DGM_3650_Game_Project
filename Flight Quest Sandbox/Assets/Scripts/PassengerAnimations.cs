ede210d79d75e5d2d1ec60b414f11c5cd27fa5d6
Rhett <thedovahbrine@gmail.com>
2019-10-03 04:09:47 -0600

    Working

---
+++ Flight Quest Sandbox/Assets/Scripts/PassengerAnimations.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class PassengerAnimations : MonoBehaviour
+{
+    public Animator animator;
+    private void Update()
+    {
+        animator.SetBool("Walk", true);
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.name == "sitLoc")
+        {
+            //animator.SetBool("Walk", false);
+            Debug.Log("Working");
+        }
+    }
+}
---
+++ Flight Quest Sandbox/Assets/test.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class test : MonoBehaviour
+{
+ void OnCollisionEnter(Collision other)
+    {
+        if(other.gameObject.name == "Tester")
+        {
+            Debug.Log("HELLLLLLLLLLOOO");
+        }
+    }
+}
