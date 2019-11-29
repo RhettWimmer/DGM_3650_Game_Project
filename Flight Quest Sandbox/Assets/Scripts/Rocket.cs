016d94f59652828193919cfbb8a55ff824599cb6
Rhett <thedovahbrine@gmail.com>
2019-11-29 01:33:25 -0700

    uving stuff

---
+++ Flight Quest Sandbox/Assets/Scripts/Rocket.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rocket : MonoBehaviour
+{
+    public int moveSpeed;
+    void Update()
+    {
+        if (Input.GetKeyDown("space"))
+        {
+            var move = Time.deltaTime * moveSpeed;
+            transform.Translate(0, 10, 0);
+        }      
+    }
+}
